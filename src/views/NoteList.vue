d1f9770f81280f97f95eb46aa88a1e695e71bb10
oxygene4 <5797565@gmail.com>
2020-06-15 21:56:47 +0300

    initial

---
+++ src/views/NoteList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="nav">
+        <router-link :to="{ name: 'note', params: { id: 1 } }">1</router-link>
+        <br>
+        <router-link :to="{ name: 'note', params: { id: 2 } }">2</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHome',
+};
+</script>
---
+++ src/views/TheNote.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+        <router-link to="/">Home</router-link>
+        <h1>This is an about note {{ $route.params.id }}</h1>
+    </div>
+</template>
